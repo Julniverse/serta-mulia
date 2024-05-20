6bc4a3b10b668414fa5e496de042321b23bb6e0f
Juliandi Kurniansyah <juliandikurniansyah@gmail.com>
2024-05-20 13:43:47 +0700

    Push code to remote repository

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,5 +1,6 @@
 const predictClassification = require("../services/inferenceService");
 const crypto = require("crypto");
+const storeData = require("../services/storeData");
 
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
@@ -18,7 +19,7 @@ async function postPredictHandler(request, h) {
     confidenceScore: confidenceScore,
     createdAt: createdAt,
   };
-
+  await storeData(id, data);
   const response = h.response({
     status: "success",
     message:
